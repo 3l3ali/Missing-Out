5bf8b1d1c7be52bede4570affd1863b0036c26c3
khalid abubakr <khalid7.bkr@gmail.com>
2017-07-18 11:07:49 +0900

    added photo to Post and did the views for posts with forms

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ApplicationRecord
+  mount_uploader :photo, PhotoUploader
+
   belongs_to :user
   belongs_to :location
-
   has_many :reviews
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170717033645) do
+ActiveRecord::Schema.define(version: 20170718020617) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170717033645) do
     t.integer  "location_id"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "photo"
     t.index ["location_id"], name: "index_posts_on_location_id", using: :btree
     t.index ["user_id"], name: "index_posts_on_user_id", using: :btree
   end
