6f0d6367bc1f00a804c5442d59fd0df168f177e4
Mohammad Alali <m.a.alsyouf@gmail.com>
2017-07-18 10:16:22 +0900

    changed routes to non nested

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,8 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :users do
-    resources :posts
-    resources :reviews
-  end
+  resources :users
+  resources :posts
+  resources :reviews
 
 end
