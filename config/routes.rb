75bc0fb320693436b2970be52faa2f9453bf893a
khalid abubakr <khalid7.bkr@gmail.com>
2017-07-19 18:53:44 +0900

    user is able to follow other users from show page and added avatar next to review poster

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'followings/create'
+
+  get 'followings/destroy'
+
   devise_for :users,
     controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root to: 'pages#home'
