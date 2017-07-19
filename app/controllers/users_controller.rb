9c40d8664d862a9b05850ed64f39a0504a7b6894
khalid abubakr <khalid7.bkr@gmail.com>
2017-07-19 14:20:36 +0900

    added pundit for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,13 +2,16 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
-    @users = User.all
+    @users = policy_scope(User).order(created_at: :desc)
+    # @users = User.all
   end
 
   def show
+    authorize @user
   end
 
   def destroy
+    authorize @user
     @user.destroy
     redirect_to root_path
   end
